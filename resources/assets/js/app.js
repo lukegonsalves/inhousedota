10e7a4f325e0d025519fbdf07139534ad0d31993
lukegonsalves <darkluke210@hotmail.co.uk>
2018-04-04 10:42:34 +0100

    clean install

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,18 +5,23 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
 
 window.Vue = require('vue');
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+
+Vue.use(Buefy)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
+/*
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
     el: '#app'
 });
+*/
