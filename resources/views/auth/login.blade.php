20168e98a91533b6799aa7563d5c8cefe3a4f376
lukegonsalves <darkluke210@hotmail.co.uk>
2018-05-17 13:51:05 +0100

    updated login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="section">
-        <div class="container">
-            <center>
-                Sign in with Steam
-                <a href="auth/steam">
-                    <br><br><img src="http://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_large_noborder.png" width="109px" height="66px" border="0">
-                </a>
-            </center>
+    <section class="hero is-light is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                        <h3 class="title has-text-grey">Inhouse Dota Login</h3>
+
+                    <div class="box">
+                            <p class="subtitle has-text-grey">Please Sign in with Steam to proceed.</p>
+                            <a href="auth/steam">
+                                <img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_02.png" width="109" height="66" border="0">
+                            </a>
+                    </div>
+                    
+                    <p class="has-text-grey has-text-bold">
+                        <a href="../">View our privacy policy</a>
+                    </p>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
