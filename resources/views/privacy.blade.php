af4e3aa718ab234e8e705ea1dea34c80eaa1ab08
lukegonsalves <darkluke210@hotmail.co.uk>
2018-05-22 21:42:39 +0100

    updated privacy policy page
    added the privacy policy to the website

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,9 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="hero is-dark is-fullheight">
-    Privacy Policy
-
+<section class="hero is-fullheight">
+    <div class="hero-body">
+    <div class="columns is-centered">
+        <div class="column is-half">
+            <div class="box">
+                <div class="content">
+                    <h1 class = "has-text-centered is-capitalized has-text-weight-semibold">privacy policy</h1>
+                    <h6 class = "subtitle has-text-centered">Last updated: 22 May 2018</h6>
+                    <p>This privacy policy describes how InhouseDota (the "company") collects, uses and discloses information,
+                     and what choices you have with respect to that information and our compliance with the <a target="_blank" href="https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=celex%3A32016R0679">General Data Protection Regulation</a>. We encourage you to fully read the following privacy policy statement.</p>
+                    <br><h5 class="has-text-weight-semibold">Information We Collect And Receive</h5>
+                    <p>We collect User Information data about the user:
+                        <ul>
+                            <li><strong>Account Information.</strong> To login to this site, Users must do so with a third party "Steam" account. Therefore we store the identities and related metadata for your third party account, this includes related data from the Dota API. However, this does <strong>not</strong> include email addresses or passwords.</li>
+                            <li><strong>User Submitted Content.</strong> Users may choose to submit original content like match names, and profile customisations, and other user created or submitted content. We collect and store this information on our servers.</li>
+                        </ul>
+                    </p>
+                    <br><h5 class="has-text-weight-semibold">How We Use Information</h5>
+                    <p>We use the information collected for the following purposes:
+                        <ul>
+                            <li><strong>To provide, update, maintain, and operate the website.</strong> We use User information to provide customised features for you on the website.</li>
+                            <li><strong>To develop and provide additional features on the website.</strong> We use User information to aide in making decisions about the future of the website, such as making improvements or creating new features.</li>
+                            <li><strong>To investigate and help prevent security issues and abuse.</li>
+                            <li>For other purposes as required by applicable law, legal process or regulation.</strong></li>
+                        </ul>
+                    </p>
+                    <br><h5 class="has-text-weight-semibold">How We Share and Disclose Information</h5>
+                    <p>
+                        <ul>
+                            <li><strong>Public Display on the website.</strong> We may display User Information to other users of the website. For example, by logging in and therefore consenting the Company to access related metadata for your account, we may show it to other visitors.</li>
+                            <li><strong>Third Party Service Providers and Partners.</strong> We may engage third party companies or individuals as service providers or business partners to process data to support our business. These third parties may, for example, provide virtual computing, storage, payment, help desk, bug tracking, or other related services.</li>
+                            <li><strong>Corporate Affiliates and Partners.</strong> We may share User Information with corporate affiliates, parents and/or subsidiaries. If the Company engages in a merger, acquisition, bankruptcy, dissolution, reorganization, sale of some or all of the Company's assets or stock, financing, public offering of securities, acquisition of all or a portion of our business, a similar transaction or proceeding, or steps in contemplation of such activities (e.g. due diligence), some or all data may be shared or transferred, subject to standard confidentiality arrangements.</li>
+                            <li><strong>Aggregated or Anonymized Data.</strong> We may disclose or use aggregated or de-identified data for any purpose. For example, we may share aggregated or de-identified account, usage, and log with prospects or partners for business or research purposes.</li>
+                            <li><strong>Upon Request.</strong> We may share or disclose User Information in accordance with the instructions of the User, and in compliance with applicable law and legal process.</li>
+                            <li><strong>To Comply with Laws.</strong> If we receive a request for information, we may disclose User, Customer, or Other Information if we reasonably believe disclosure is in accordance with or required by any applicable law, regulation or legal process.</li>
+                            <li><strong>To enforce our rights, prevent fraud, and for safety.</strong> We may use data to protect and defend the rights, property or safety of the Company or third parties, including enforcing contracts or policies, or in connection with investigating and preventing fraud or security issues.</li>
+                            <li><strong>With Consent.</strong> We may share data with third parties when we have consent to do so.</li>
+                        </ul>
+                    </p>
+                    <br><h5 class="has-text-weight-semibold">Security</h5>
+                    <p>We take security of data very seriously and work hard to protect Information you provide from loss, misuse, and unauthorized access or disclosure. These steps take into account the sensitivity of the Other Information we collect, process and store, and the current state of technology. Given the nature of communications and information processing technology, we cannot guarantee that Information, during transmission through the Internet or while stored on our systems or otherwise in our care, will be absolutely safe from intrusion by others.
+                    </p>
+                    <br><h5 class="has-text-weight-semibold">Changes To This Privacy Policy</h5>
+                    <p>
+                    We may change this Privacy Policy from time to time. Laws, regulations and industry standards evolve, which may make those changes necessary, or we may make changes to our business. We will post the changes to this page and encourage you to review our Privacy Policy to stay informed. If we make changes that materially alter your privacy rights we will provide notice on the website. If you disagree with the changes to this Privacy Policy, you should deactivate your website account.
+                    </p>
+                    <br><h5 class="has-text-weight-semibold">Cookies</h5>
+                    <p>A cookie is a small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site’s computers and stored on your computer’s hard drive. Our website uses cookies to record current session information. Our cookies typically exist perpetually and we provide the ability to remove your session cookie with a sign out link. You can remove or block cookies in your browser settings.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 </section>
 
 @endsection
