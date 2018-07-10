3baae17cc452a1a28e17384f11a8502f2568a80c
lukegonsalves <darkluke210@hotmail.co.uk>
2018-07-10 21:03:13 +0100

    test push to server 2

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -18,7 +18,7 @@
                     Hi all,<br>
                     Just a quick one! For me this is very much a passion project and I try to commit as much time as possible, where possible to 
                     developing this inhouse site. However, being a student I'm skint! If you could spare
-                    any shillings it would be gratefully received, contributing to help us to pay for servers, improve and develop the site to take it to the next level! 
+                    any shillings they would be gratefully received, contributing to help us to pay for servers, improve and develop the site to take it to the next level! 
                     <br>So, for those of you who <strong>really really really</strong> want to donate you can do so below!
                     <br><br>
                     <center><a class="button is-large is-rounded" target="_blank" href="https://www.buymeacoffee.com/8E37jwZgX"><img src="https://www.buymeacoffee.com/assets/img/BMC-btn-logo.svg" alt="Buy me a coffee"><span style="margin-left:5px">Buy me a coffee</span></a></center>
