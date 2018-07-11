7db9650afd0b552ddbc52e1423b2a48ddbb2c215
David Piesse <piesse@gmail.com>
2018-07-11 09:23:42 +0100

    Update 2018_07_10_215341_add_status_field.php

--- database/migrations/2018_07_10_215341_add_status_field.php
+++ database/migrations/2018_07_10_215341_add_status_field.php
@@ -14,7 +14,7 @@ class AddStatusField extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('status');
+            $table->string('status')->nullable();
         });
     }
 
