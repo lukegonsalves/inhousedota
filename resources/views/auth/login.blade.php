67b522197433eac5b49c153c6c2ef7f329b70bc5
lukegonsalves <darkluke210@hotmail.co.uk>
2018-08-31 13:06:16 +0100

    login blade update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,38 +3,31 @@
 @section('content')
     <section class="hero is-fullheight">
         <div class="hero-body">
-            <div class="container has-text-centered">
+            <div class="container has-text-centered border-teal">
                 <div class="columns is-centered">
-                    <figure class="image">
+                    <!-- <figure class="image">
                             <img src="https://i.ytimg.com/vi/pWN3x2iyIkE/maxresdefault.jpg">
-                    </figure>
+                    </figure> -->
                     <div class="column is-one-third">
-                    <div class="box">
-                            <h3 class="title has-text-grey">Inhouse Dota Login</h3>
-                            <p class="subtitle has-text-grey">Please Sign in with Steam to proceed.</p>
+                    <div class="bg-indigo-darker shadow-md rounded px-8 pt-6 pb-8 mb-4 border-teal border">
+                            <div class="text-grey-lightest font-semibold uppercase opacity-75">Inhouse Dota Login</div>
+                            <hr class="mb-4 mt-6 border border-indigo-dark opacity-25">
+                            <p class="text-sm text-grey leading-normal opacity-75 mb-4">Please Sign in with Steam to proceed.</p>
                             <a href="auth/steam">
                                 <img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_02.png" width="109" height="66" border="0">
                             </a>
                     </div>
+                    <p class="text-center text-grey text-xs">
+                        ©2018 Inhouse Dota. All rights reserved.
+                    </p>
                     </div>
-                    <figure class="image">
+                    <!-- <figure class="image">
                             <img src="https://i.ytimg.com/vi/yb9IHDPR_F4/maxresdefault.jpg">
-                    </figure>
+                    </figure> -->
                 </div>
-            </div>
-        </div>
-<div class="w-full max-w-xs">
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 
-    <div class="flex items-center justify-between">
-        <a href="auth/steam">
-            <img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_02.png" width="109" height="66" border="0">
-        </a>
-    </div>
-  </form>
-  <p class="text-center text-grey text-xs">
-    ©2018 Inhouse Dota. All rights reserved.
-  </p>
-</div>
+            </div>
+
+        </div>
     </section>
 @endsection
